ae6589185cd262d4971bce00d9c8aa1f1fd374b8
BE1A <1k8dcds@gmail.com>
2017-04-13 19:00:45 +0900

    Create UpdateTest.user.js

---
+++ UpdateTest.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         TestScript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       BE1A
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
